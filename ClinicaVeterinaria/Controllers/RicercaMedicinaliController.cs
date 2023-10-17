6cab9784125c47a98e2ae8ec0f2be528c47cc216
Erica Diana <134380180+EricaDianaa@users.noreply.github.com>
2023-10-17 17:24:28 +0200

    CercaMedicinali

---
+++ ClinicaVeterinaria/Controllers/RicercaMedicinaliController.cs
@@ -0,0 +1,27 @@
+﻿using ClinicaVeterinaria.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ClinicaVeterinaria.Controllers
+{
+
+    public class RicercaMedicinaliController : Controller
+    {  
+        ModelDBContext db = new ModelDBContext();
+        // GET: RicercaMedicinali
+        public ActionResult RicercaMedicinale()
+        {
+            return View();
+        }
+       // [HttpPost]
+        public JsonResult RicercaMedicinaleData(DateTime data)
+        {
+           int Totale = db.Vendite.Count(m => m.DataVendita == data);
+
+            return Json(Totale);
+        }
+    }
+}
