5cd06effd7c47cebc50d1488090b323982514eae
Erica Diana <134380180+EricaDianaa@users.noreply.github.com>
2023-10-17 18:34:38 +0200

    Inizio Query Medicinali from CF

--- ClinicaVeterinaria/Controllers/RicercaMedicinaliController.cs
+++ ClinicaVeterinaria/Controllers/RicercaMedicinaliController.cs
@@ -23,5 +23,18 @@ namespace ClinicaVeterinaria.Controllers
 
             return Json(Totale);
         }
+        [HttpPost]
+        public JsonResult RicercaMedicinaleCF(string CodiceFiscale)
+        {
+            Utenti user = db.Utenti.FirstOrDefault(m => m.CodiceFiscale == CodiceFiscale);
+            List<Vendite> vendita= db.Vendite.Where(m => m.IdUtente == user.IdUtente).ToList();
+            List<Vendite> v = new List<Vendite>();
+            foreach (Vendite ve in vendita)
+            {
+                v.Add(ve);
+            }
+            return Json(v);
+        }
+
     }
 }
