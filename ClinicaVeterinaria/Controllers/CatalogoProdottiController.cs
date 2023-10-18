1ac6730e42ca176b186366de76a2ab1ddce119bc
fedemaso <fede.maso1989@gmail.com>
2023-10-18 11:32:42 +0200

    stile admin menu view e view catalogo prodotti

---
+++ ClinicaVeterinaria/Controllers/CatalogoProdottiController.cs
@@ -0,0 +1,27 @@
+﻿using ClinicaVeterinaria.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ClinicaVeterinaria.Controllers
+{
+    public class CatalogoProdottiController : Controller
+    {
+        private ModelDBContext _context;
+
+        public CatalogoProdottiController()
+        {
+            _context = new ModelDBContext();
+        }
+
+        // GET: CatalogoProdotti
+        public ActionResult Index()
+        {
+            var prodotti = _context.Prodotti.Include("Ditte").ToList();
+            return View(prodotti);
+        }
+
+    }
+}
