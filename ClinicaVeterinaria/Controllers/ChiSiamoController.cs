bda8fe0a893d5d0f06d15162a638d4f67732a1b6
fedemaso <fede.maso1989@gmail.com>
2023-10-19 11:29:19 +0200

    sistemato card razze home

---
+++ ClinicaVeterinaria/Controllers/ChiSiamoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ClinicaVeterinaria.Controllers
+{
+    public class ChiSiamoController : Controller
+    {
+        // GET: ChiSiamo
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
