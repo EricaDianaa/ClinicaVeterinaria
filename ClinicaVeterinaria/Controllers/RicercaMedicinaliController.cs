5f6444c90a140d00630c88ea4cf2fa4a738c4792
AlessioDiPretoro <alessiodipretoro@gmail.com>
2023-10-19 15:44:29 +0200

    Ricerche

--- ClinicaVeterinaria/Controllers/RicercaMedicinaliController.cs
+++ ClinicaVeterinaria/Controllers/RicercaMedicinaliController.cs
@@ -32,27 +32,36 @@ namespace ClinicaVeterinaria.Controllers
         [HttpPost]
         public JsonResult RicercaMedicinaleCF(string CodiceFiscale)
         {
-            
             Utenti user = db.Utenti.FirstOrDefault(m => m.CodiceFiscale == CodiceFiscale);
-            List<Vendite> vendita= db.Vendite.Where(m => m.IdUtente == user.IdUtente).ToList();
-            List<Vendite> v = new List<Vendite>();
+            List<Vendite> vendita = db.Vendite.Where(m => m.IdUtente == user.IdUtente).ToList();
+            List<VenditeDaCodFisc> v = new List<VenditeDaCodFisc>();
+            List<Pr> prodottiList = new List<Pr>();
             foreach (Vendite ve in vendita)
             {
-                v.Add(new Vendite { IdVendita = ve.IdVendita, IdUtente = ve.IdUtente, DateVenditaString = ve.DataVendita.ToShortDateString().ToString(), NumeroRicetta = ve.NumeroRicetta });
+                foreach (Dettagli dettagli in ve.Dettagli)
+                {
+                    prodottiList.Add(new Pr { Nome = dettagli.Prodotti.Nome, Qta = dettagli.Quantita, Prezzo = dettagli.Prezzo });
+                }
+                //v.Add(new Vendite { IdVendita = ve.IdVendita, IdUtente = ve.IdUtente, DateVenditaString = ve.DataVendita.ToShortDateString().ToString(), NumeroRicetta = ve.NumeroRicetta });
+                v.Add(new VenditeDaCodFisc
+                {
+                    DataAcquisto = ve.DataVendita.ToShortDateString().ToString(),
+                    NumeroRicetta = ve.NumeroRicetta,
+                    ProdottiLista = prodottiList
+                });
             }
             return Json(v);
         }
 
-      
         [HttpPost]
         public JsonResult RicercaMedicinaleCasetto(int IdProdotti)
         {
             ViewBag.Prodotti = new SelectList(db.Prodotti, "Prodotti", "Nome");
-            Cassetto_Prodotti cassetto= db.Cassetto_Prodotti.Where(M=>M.IdProdotti == IdProdotti).FirstOrDefault();
-            Cassetti IdArmadietti = db.Cassetti.Where(M => M.IdCassetto ==cassetto.IdCassetto).FirstOrDefault();
-            Armadietti armadietto=db.Armadietti.Where(m=>m.IdArmadietto==IdArmadietti.IdArmadietto).FirstOrDefault();
-            Prodotti p =db.Prodotti.Where(M => M.IdProdotto == IdProdotti).FirstOrDefault();
-            Cassetto_Prodotti cassetto1 = new Cassetto_Prodotti { IdProdotti = cassetto.IdProdotti, IdCassetto = cassetto.IdCassetto, NomeCassetto = cassetto.Cassetti.NomeCassetto,Armadietto=armadietto.CodiceArmadietto,NomeMedicinale=p.Nome };
+            Cassetto_Prodotti cassetto = db.Cassetto_Prodotti.Where(M => M.IdProdotti == IdProdotti).FirstOrDefault();
+            Cassetti IdArmadietti = db.Cassetti.Where(M => M.IdCassetto == cassetto.IdCassetto).FirstOrDefault();
+            Armadietti armadietto = db.Armadietti.Where(m => m.IdArmadietto == IdArmadietti.IdArmadietto).FirstOrDefault();
+            Prodotti p = db.Prodotti.Where(M => M.IdProdotto == IdProdotti).FirstOrDefault();
+            Cassetto_Prodotti cassetto1 = new Cassetto_Prodotti { IdProdotti = cassetto.IdProdotti, IdCassetto = cassetto.IdCassetto, NomeCassetto = cassetto.Cassetti.NomeCassetto, Armadietto = armadietto.CodiceArmadietto, NomeMedicinale = p.Nome };
             return Json(cassetto1);
         }
     }
