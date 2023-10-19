544baec816828f988e83cdcc6c0f33f7a896a656
AlessioDiPretoro <alessiodipretoro@gmail.com>
2023-10-19 13:01:00 +0200

    RicercaDaAnonimoModifcaNavUserEAdmin

--- ClinicaVeterinaria/Controllers/HomeController.cs
+++ ClinicaVeterinaria/Controllers/HomeController.cs
@@ -9,65 +9,84 @@ namespace ClinicaVeterinaria.Controllers
 {
     public class HomeController : Controller
     {
-        ModelDBContext db = new ModelDBContext();
+        private ModelDBContext db = new ModelDBContext();
+
         public ActionResult Index()
         {
             ViewBag.Title = "Home Page";
 
             return View();
         }
-        
-       // -------------RicercaAnimale----------
+
+        // -------------RicercaAnimale----------
         public ActionResult RicercaAnimale()
         {
             Session["Animale"] = null;
             Session["NessunAnimale"] = null;
             return View();
         }
+
         [HttpPost]
         public JsonResult RicercaAnimale1(AnimaliRicoverati animale)
         {
-
             AnimaliRicoverati a = db.AnimaliRicoverati.FirstOrDefault(m => m.NumeroMicrochip == animale.NumeroMicrochip);
             Session["Animale"] = a;
 
             List<AnimaliRicoverati> animal = new List<AnimaliRicoverati>();
             if (a != null)
             {
-            animal.Add(new AnimaliRicoverati {Foto=a.Foto, CognomeProprietario=a.CognomeProprietario,NomeProprietario=a.NomeProprietario,Nome=a.Nome,IdAnimali=a.IdAnimali,IdUtente=a.IdUtente,DataInizioRicovero=a.DataInizioRicovero, DataInizioRicoveroString = a.DataInizioRicovero?.ToShortDateString().ToString(), DataNascita=a.DataNascita,DataNascitaString= a.DataNascita?.ToShortDateString().ToString(),DataregistrazioneString=a.DataRegistrazione.ToShortDateString().ToString(), ColoreMantello=a.ColoreMantello,TipologiaNome=a.Tipologia1.Nome});
-            ViewBag.Nome = a.Tipologia1.Nome;
+                animal.Add(new AnimaliRicoverati { Foto = a.Foto, CognomeProprietario = a.CognomeProprietario, NomeProprietario = a.NomeProprietario, Nome = a.Nome, IdAnimali = a.IdAnimali, IdUtente = a.IdUtente, DataInizioRicovero = a.DataInizioRicovero, DataInizioRicoveroString = a.DataInizioRicovero?.ToShortDateString().ToString(), DataNascita = a.DataNascita, DataNascitaString = a.DataNascita?.ToShortDateString().ToString(), DataregistrazioneString = a.DataRegistrazione.ToShortDateString().ToString(), ColoreMantello = a.ColoreMantello, TipologiaNome = a.Tipologia1.Nome });
+                ViewBag.Nome = a.Tipologia1.Nome;
             }
             else
             {
                 Session["NessunAnimale"] = "null";
             }
-           
+
             return Json(animal);
         }
 
+        //ricerca le visite collegate all'animale
         public JsonResult RicercaAnimale2()
         {
-          AnimaliRicoverati a = (AnimaliRicoverati) Session["Animale"];
-             List<VisiteVeterinarie> v= new List<VisiteVeterinarie>();
+            AnimaliRicoverati a = (AnimaliRicoverati)Session["Animale"];
+            List<VisiteVeterinarie> v = new List<VisiteVeterinarie>();
             List<VisiteVeterinarie> visite = new List<VisiteVeterinarie>();
             if (a != null)
-            {  
-                AnimaliRicoverati an = db.AnimaliRicoverati.FirstOrDefault(m => m.NumeroMicrochip == a.NumeroMicrochip);
-               int ID = an.IdAnimali;
-               v = db.VisiteVeterinarie.Where(m => m.IdAnimale == ID).ToList();
-           
-            foreach(VisiteVeterinarie vis in v)
             {
-              visite.Add(new VisiteVeterinarie { Descrizione= vis.Descrizione,idVisite=vis.idVisite,IdAnimale=vis.IdAnimale,Data=vis.Data,DataString=vis.Data.ToShortDateString().ToString()});
-            }
-          
+                AnimaliRicoverati an = db.AnimaliRicoverati.FirstOrDefault(m => m.NumeroMicrochip == a.NumeroMicrochip);
+                int ID = an.IdAnimali;
+                v = db.VisiteVeterinarie.Where(m => m.IdAnimale == ID).ToList();
+
+                foreach (VisiteVeterinarie vis in v)
+                {
+                    visite.Add(new VisiteVeterinarie { Descrizione = vis.Descrizione, idVisite = vis.idVisite, IdAnimale = vis.IdAnimale, Data = vis.Data, DataString = vis.Data.ToShortDateString().ToString() });
+                }
             }
 
-            return Json(visite,JsonRequestBehavior.AllowGet);
+            return Json(visite, JsonRequestBehavior.AllowGet);
         }
-      
-        //--------------------------------------
 
+        //--------------------------------------
+        //------------------RICECA DA ANONIMI--------------------
+
+        public ActionResult RicercaAnonimaDaChip()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult RicercaAnonimaDaChip(AnimaliRicoverati animale)
+        {
+            AnimaliRicoverati a = db.AnimaliRicoverati.FirstOrDefault(m => m.NumeroMicrochip == animale.NumeroMicrochip);
+
+            List<AnimaliRicoverati> animal = new List<AnimaliRicoverati>();
+            if (a != null)
+            {
+                animal.Add(new AnimaliRicoverati { Foto = a.Foto, CognomeProprietario = a.CognomeProprietario, NomeProprietario = a.NomeProprietario, Nome = a.Nome, IdAnimali = a.IdAnimali, IdUtente = a.IdUtente, DataInizioRicovero = a.DataInizioRicovero, DataInizioRicoveroString = a.DataInizioRicovero?.ToShortDateString().ToString(), DataNascita = a.DataNascita, DataNascitaString = a.DataNascita?.ToShortDateString().ToString(), DataregistrazioneString = a.DataRegistrazione.ToShortDateString().ToString(), ColoreMantello = a.ColoreMantello, TipologiaNome = a.Tipologia1.Nome });
+                ViewBag.Nome = a.Tipologia1.Nome;
+            }
+            return Json(animal);
+        }
     }
 }
-
